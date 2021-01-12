04d1bf71347d2f10b4c7f3e51dd33a9359537495
Raymond Tran <raytran@mit.edu>
2021-01-12 17:51:10 -0500

    Replaced flag addressing with timestamp system

--- src/baseplayer/flags/FlagType.java
+++ src/baseplayer/flags/FlagType.java
@@ -1,9 +1,2 @@
-package baseplayer.flags;
-
-public enum FlagType {
-    INVALID, // not used, must be here so default flag works
-    ENEMY_SPOTTED,
-    NEUTRAL_EC_SPOTTED,
-    BOUNDARY_SPOTTED;
-    public static final FlagType[] values = values();
+package baseplayer.flags;public class FlagType {
 }
